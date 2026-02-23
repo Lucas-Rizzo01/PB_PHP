98fa020748a20f01ab3999a982c92fe4cbea974c
Lucas <lucas.bertoloto@aluno.senai.br>
2026-02-23 08:08:54 -0300

    atividade exemplo

---
+++ mvc_exemplo/View/usuarioCadastrar.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    
+    <title>Document</title>
+</head>
+<body>
+    <a href="/PB_PHP/mvc_exemplo/usuario/listar">Ir para tela listar</a>
+    <form method="POST" action="salvar">
+        <input type="text" name="nome" placeholder="Seu nome" require>
+        <input type="text" name="email" placeholder="Seu email" require>
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
---
+++ mvc_exemplo/View/usuarioListar.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt_BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Usuarios</title>
+</head>
+<body>
+    <a href="/PB_PHP/mvc_exemplo/usuario/telaCadastro">Voltar para tela de cadastro</a>
+    <h2>Usuários</h2>
+    <table border ="1">
+        <tr>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Ações</th>
+        </tr>
+        <?php foreach ($usuarios as $u): ?>
+            <tr>
+                <td><?=$u['nome']?></td>
+                <td><?=$u['email']?></td>
+                <td>Proxima aula</td>
+            </tr>
+            <?php endforeach; ?>
+    </table>
+</body>
+</html>
